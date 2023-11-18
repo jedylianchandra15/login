edae2d00ebb7a8a7769dd1b902ab5aaefd32cd0d
jedylianchandra15 <148891788+jedylianchandra15@users.noreply.github.com>
2023-11-17 17:53:37 -1200

    Create login.php
    menambahkan form login

---
+++ login.php
@@ -0,0 +1,102 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Retania | KOST</title>
+
+  <link href="img/SVG/Logo V!-1.svg" rel="icon">
+  <link href="img/SVG/Logo V!-1.svg" rel="apple-touch-icon">
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="AdminLTE/plugins/fontawesome-free/css/all.min.css">
+  <!-- icheck bootstrap -->
+  <link rel="stylesheet" href="AdminLTE/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="AdminLTE/dist/css/adminlte.min.css">
+</head>
+<style>
+    body {
+        background: url("img/bg.jpg") no-repeat center center fixed;
+        -webkit-background-size: cover;
+        -moz-background-size: cover;
+        -o-background-size: cover;
+        background-size: cover;
+    }
+</style>
+<body class="hold-transition login-page">
+<div class="login-box">
+  <!-- /.login-logo -->
+  <div class="card card-outline card-purple">
+  <?php
+            if (isset($_POST['submit'])) {
+                include "koneksi.php";
+                $username = $_POST['username'];
+                $pass = $_POST['password'];
+                $query = mysqli_query($conn, "SELECT * FROM user WHERE username = '$username' AND password = '$pass'");
+                $row = mysqli_num_rows($query);
+                if ($row > 0) {
+                    $data = mysqli_fetch_assoc($query);
+                    $_SESSION['id_user'] = $data['id_user'];
+                    $_SESSION['username'] = $data['username'];
+                    $_SESSION['password'] = $data['password'];
+                    $_SESSION['tipe'] = $data['tipe'];
+                    header('Location:index.php');
+                } else {
+                    echo '<div class="alert bg-pink alert-dismissible" role="alert">
+          <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+          Username atau Password Salah !!!
+      </div>';
+                }
+            }
+            ?>
+    <div class="card-header text-center">
+      <a href="" class="h1"><b>Retania</b>Kost</a>
+    </div>
+    <div class="card-body">
+      <p class="login-box-msg"><b>Administrator Login</b></p>
+      <br><center><p>Repost by <a href='https://stokcoding.com/' title='StokCoding.com' target='_blank'>StokCoding.com</a></p></center>
+
+
+      <form action="" method="post" enctype="multipart/form-data">
+        <div class="input-group mb-3">
+          <input type="text" name="username" class="form-control" value="admin" placeholder="Username">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-user"></span>
+            </div>
+          </div>
+        </div>
+        <div class="input-group mb-3">
+          <input type="password" name="password" class="form-control" value="admin" placeholder="Password">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-lock"></span>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-12">
+            <button type="submit" name="submit" class="btn btn-warning btn-block" style="color: whitesmoke;">Sign In</button>
+          </div>
+        </div>
+      </form>
+    </div>
+    <!-- /.card-body -->
+  </div>
+  <!-- /.card -->
+</div>
+<!-- /.login-box -->
+
+<!-- jQuery -->
+<script src="AdminLTE/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="AdminLTE/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="AdminLTE/dist/js/adminlte.min.js"></script>
+</body>
+</html>
